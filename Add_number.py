a64c4482987aab9f5a03604d7f469f72add0ae8b
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2024-05-15 16:40:51 +0530

    Update Add_number.py

--- Add_number.py
+++ Add_number.py
@@ -2,3 +2,12 @@
 num1 = int(input("Enter the first number: ")
 num2 = int(input("Enter the second number: ")
 print("Result is: ",num1+num2)
+
+
+    # Add two number use in function
+def Add(a,b):
+  c = a+b
+  print("Result is : ",c)
+num1 = int(input("Enter the first number: ")
+num2 = int(input("Enter the second number: ")
+Add(num1,num2)
