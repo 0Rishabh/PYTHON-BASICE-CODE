7c0bc7aedd931a7069cf7f2d52cb5594b0ac43c1
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2024-05-19 17:49:12 +0530

    Create convert_km_to_miles.py

---
+++ convert_km_to_miles.py
@@ -0,0 +1,8 @@
+
+# WAP  to convert kilometers to miles.
+km =float(input("Enter distance in kilometers: "))
+   # conversion factor : 1 kilometer = 0.621371 miles
+
+conversion_factor = 0.621371
+miles = conversion_factor*km
+print(f"{km} kilometers is equal to {miles} miles.")
