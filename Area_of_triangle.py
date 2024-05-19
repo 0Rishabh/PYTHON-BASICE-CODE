08a739ae0cbee8f49332ed4c3beb080798986144
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2024-05-19 17:36:26 +0530

    Create Area_of_triangle.py

---
+++ Area_of_triangle.py
@@ -0,0 +1,11 @@
+
+# WAP to find the area of a triangle.
+
+base = float(input("Enter the length of the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+                # calculate the area of the triangle.
+area = 0.5 * base * height  
+
+    # Display the result
+print(f"The area of a triangle is:{area}") 
+
