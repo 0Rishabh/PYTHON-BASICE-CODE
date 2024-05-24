9ba66cb3735c3642a318ede3c19dea6a76ede1ef
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2024-05-24 22:10:02 +0530

    Create check_num_positive_negative_zero.py

---
+++ check_num_positive_negative_zero.py
@@ -0,0 +1,10 @@
+
+# WAP to check if a number is positive, negative or zero 
+
+num = float(input("Enter a number: "))
+if(num > 0):
+    print("The number is positive:")
+elif(num==0):
+    print("The number is zero:")
+else:
+    print("The number is negative:")
