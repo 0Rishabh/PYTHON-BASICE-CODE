0bd5900603d8cace31ed61a2a92f82cb0dcc0782
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2024-05-26 13:08:44 +0530

    Create decimal_to_bin_oct_hexa_convert_.py

---
+++ decimal_to_bin_oct_hexa_convert_.py
@@ -0,0 +1,10 @@
+
+# Write a Python Program to Convert Decimal to Binary,
+                            #   Octal and Hexadecimal.
+                              
+dec_num = int(input('Enter a decimal number: '))
+print("The decimal value of", dec_num, "is:")
+print(bin(dec_num), "in binary.")
+print(oct(dec_num), "in octal.")
+print(hex(dec_num), "in hexadecimal.")
+    
