df9856ce9ada4c870b391a1f48f8178b0dab9d3e
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2024-05-26 13:49:00 +0530

    Create simple_calculator_4_basic_opera.py

---
+++ simple_calculator_4_basic_opera.py
@@ -0,0 +1,68 @@
+
+ #   Write a Python Program to Make a Simple Calculator 
+        # with 4 basic mathematical operations
+
+
+
+  # This function add two numbers
+def add(x,y):
+    return x + y
+
+  # This function subtracts two numbers
+def subtract(x , y):
+    return x - y 
+
+  # This function multiplies two numbers
+def multiply(x , y):
+    return x * y
+
+  # This function divides two numbers
+def divide(x , y):
+    return x / y
+
+
+print("""
+      Select Operation.
+      1. Add
+      2. Subtract
+      3. Multiply
+      4. Divide
+      """)
+
+while True:
+    
+       # Take input from the user
+       choice = input("Enter your Choice(1/2/3/4): ")
+
+       # Check if choice is one of the four options:
+       if choice in ('1' , '2' , '3' , '4'):
+            try:
+                 num1 = float(input("Enter First Number: "))
+                 num2 = float(input("Enter Second Number: "))
+            except ValueError:
+                 print("Invalid input. Please Enter a number. ")
+                 continue
+            
+            if choice == '1':
+                 print(f"{num1} + {num2} = {num1+num2}")
+
+            elif choice == '2':
+                 print(f"{num1} - {num2} = {num1-num2}")
+
+            elif choice == '3':
+                 print(f"{num1} x {num2} = {num1*num2}")
+
+            elif choice == '4':
+                 print(f"{num1} / {num2} = {num1/num2}")
+
+
+      # check if user wants another calculation
+             # break the while loop if answer is no
+            next_calculation = input("Let's do next calculation..? (yes/no):")
+            
+            if next_calculation.lower() == "no":
+                 break
+            
+       else:
+            print("Invalid Input:")  
+          
