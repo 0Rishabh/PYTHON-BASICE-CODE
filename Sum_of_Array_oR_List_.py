8bded98934859e99b19846f5e39735af24dea3c8
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2024-06-06 18:39:07 +0530

    Create Sum_of_Array_oR_List_.py

---
+++ Sum_of_Array_oR_List_.py
@@ -0,0 +1,17 @@
+
+#   Write a Python Program to find sum of array.
+
+    # Using sum function:
+arr = [2 , 4 , 8 , 10 , 12]
+result = sum(arr)
+print("Sum of array or list: ",result)
+
+
+
+   # using loop
+list = [2 , 4 , 8 , 10 , 12]
+sum = 0
+for i in list:
+    sum += i
+
+print("Sum of list: ",sum)
